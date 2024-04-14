eb2d89ebe36ecf8b8b224220007425a7280a1a66
polarday <1961307973@qq.com>
2024-04-14 14:16:13 +0800

    接入mongoDB

--- server/auth/main.go
+++ server/auth/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
 	authpb "coolcar/auth/api/gen/v1"
 	"coolcar/auth/auth"
+	"coolcar/auth/dao"
 	"coolcar/auth/wechat"
 	"log"
 	"net"
 
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
@@ -22,9 +26,16 @@ func main() {
 		logger.Fatal("cannot listen", zap.Error(err))
 	}
 
+	c := context.Background()
+	mongoClient, err := mongo.Connect(c, options.Client().ApplyURI("mongodb://localhost:27017/coolcar"))
+	if err != nil {
+		logger.Fatal("cannot connect mongodb", zap.Error(err))
+	}
+
 	s := grpc.NewServer()
 	authpb.RegisterAuthServiceServer(s, &auth.Service{
 		Logger: logger,
+		Mongo:  dao.NewMongo(mongoClient.Database("coolcar")),
 		OpenIdResolver: &wechat.Service{
 			Appid:     "wxcf596336b6889e72",
 			AppSecret: "a452126e8aa608b1625571098c39358c",
